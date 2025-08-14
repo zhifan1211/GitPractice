95e09e9d910b42b0f9bbec7cce791425110c2ab7
eddy <eddy@host.docker.internal>
2025-08-14 12:00:23 +0800

    第八次測試

--- GitPractice/src/model/Test.java
+++ GitPractice/src/model/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		String str = "Hello SpongeBob";
 		String str2 = "Hello Gary";
-		System.out.println(str+", "+str2);
+		String str3 = "Hello Sandy";
+		System.out.println(str+", "+str2+", "+str3);
 	}
 
 }

5b73ce95052f78e2796f3c5b29da7544d9b284f0
eddy <eddy@host.docker.internal>
2025-08-14 11:44:31 +0800

    第六次測試

--- GitPractice/src/model/Test.java
+++ GitPractice/src/model/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		String str = "Hello world 1307";
 		System.out.println(str);
+		String str2 = "Hello TacoP";
+		System.out.println(str2);
 	}
 
 }
